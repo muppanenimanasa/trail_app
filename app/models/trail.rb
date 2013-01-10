7a62c2bddaf3b39cdafd171d8318de3175098f68
manasa <manasa@SMNDT-105.(none)>
2013-01-10 16:33:29 +0530

    trail1 commit

---
+++ app/models/trail.rb
@@ -0,0 +1,3 @@
+class Trail < ActiveRecord::Base
+  attr_accessible :address, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrailApp::Application.config.secret_token = 'dd949503129f4bead2a8faab624aecdebe85e4ef46e3f0e4e287436267088957b3006a2496115b7b66424bf061ac5c281559b04a69013d1080b618449572cd62'
